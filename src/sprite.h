ceb3ef408c5404f0000470793d02481fb6c6a701
Naum Azeredo <naumazeredo@gmail.com>
2016-09-30 17:53:03 -0300

    Removed position from sprite class

--- src/sprite.h
+++ src/sprite.h
@@ -11,29 +11,26 @@ class Sprite {
 public:
   Sprite() = default;
   Sprite(const std::string&);
-  Sprite(const std::string&, Rectf);
-  Sprite(const std::string&, Rectf, Rect);
+  Sprite(const std::string&, Vec2f);
+  Sprite(const std::string&, Vec2f, Rect);
   Sprite(Texture);
-  Sprite(Texture, Rectf);
-  Sprite(Texture, Rectf, Rect);
+  Sprite(Texture, Vec2f);
+  Sprite(Texture, Vec2f, Rect);
 
   inline Texture getTexture() const { return texture_; }
-  inline Vec2f getPos() const       { return { dest_.x, dest_.y }; }
-  inline Vec2f getSize() const      { return { dest_.w, dest_.h }; }
-  inline Rectf getDestRect() const  { return dest_; }
+  inline Vec2f getSize() const      { return size_; }
   inline Rect getClip() const       { return clip_; }
 
   void setTexture(Texture);
-  inline void setPos(Vec2f pos)       { dest_.x = pos.x; dest_.y = pos.y; }
-  inline void setSize(Vec2f size)     { dest_.w = size.x; dest_.h = size.y; }
-  inline void setDestRect(Rectf dest) { dest_ = dest; }
+  inline void setSize(Vec2f size)     { size_ = size; }
   inline void setClip(Rect clip)      { clip_ = clip; }
 
-  void draw();
+  void draw(int, int);
+  void draw(Vec2);
 
 private:
   Texture texture_;
 
-  Rectf dest_ = { 0.f, 0.f, 0.f, 0.f };
-  Rect  clip_ = { 0, 0, 0, 0 };
+  Vec2f size_ = { 0.0f, 0.0f };
+  Rect clip_  = { 0, 0, 0, 0 };
 };
