8eb1fb77bab3b36b9d7d086e7468679967202111
Naum Azeredo <naumazeredo@gmail.com>
2016-10-10 23:40:44 -0300

    Changed mouse singleton to namespace

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1,10 +1,35 @@
 /**
   mouse.cpp
- */
+  */
 #include "mouse.h"
 
-Vec2 Mouse::pos_ = { 0, 0 };
-Vec2 Mouse::relativePos_ = { 0, 0 };
+namespace Mouse {
 
-u32 Mouse::state_ = 0;
-u32 Mouse::relativeState_ = 0;
+// Internals
+namespace {
+Vec2 pos_           { 0, 0 },
+     relativePos_   { 0, 0 };
+u32  state_         { 0 },
+     relativeState_ { 0 };
+}
+
+Vec2 getPosition()         { return pos_; }
+Vec2 getRelativePosition() { return relativePos_; }
+
+bool getButtonDown(Button button)     { return state_ & button; }
+bool getButtonUp(Button button)       { return (!state_) & button; }
+bool getButtonPressed(Button button)  { return relativeState_ & state_ & button; }
+bool getButtonReleased(Button button) { return relativeState_ & (!state_) & button; }
+
+void update() {
+  Vec2 pos;
+  u32 state      = SDL_GetMouseState(&pos.x, &pos.y);
+
+  relativePos_ = { pos.x - pos_.x, pos.y - pos_.y };
+  pos_ = pos;
+
+  relativeState_ = state_ ^ state;
+  state_         = state;
+}
+
+}
--- src/mouse.h
+++ src/mouse.h
@@ -1,35 +1,23 @@
 /**
   mouse.h
- */
+  */
 #pragma once
 
 #include <SDL2/SDL_mouse.h>
 #include "util/types.h"
 
-class Mouse {
-public:
-  enum Button { LEFT = 0x01, MIDDLE = 0x02, RIGHT = 0x04, X1 = 0x08, X2 = 0x10 };
+namespace Mouse {
 
-  static inline Vec2 getPosition()         { return pos_; }
-  static inline Vec2 getRelativePosition() { return relativePos_; }
+enum Button { LEFT = 0x01, MIDDLE = 0x02, RIGHT = 0x04, X1 = 0x08, X2 = 0x10 }; 
 
-  static inline bool getButtonDown(Button button)     { return state_ & button; }
-  static inline bool getButtonUp(Button button)       { return (!state_) & button; }
-  static inline bool getButtonPressed(Button button)  { return relativeState_ & state_ & button; }
-  static inline bool getButtonReleased(Button button) { return relativeState_ & (!state_) & button; }
+Vec2 getPosition();
+Vec2 getRelativePosition();
 
-  static void update() {
-    Vec2 pos;
-    u32 state      = SDL_GetMouseState(&pos.x, &pos.y);
+bool getButtonDown(Button button);
+bool getButtonUp(Button button);
+bool getButtonPressed(Button button);
+bool getButtonReleased(Button button);
 
-    relativePos_ = { pos.x - pos_.x, pos.y - pos_.y };
-    pos_ = pos;
+void update();
 
-    relativeState_ = state_ ^ state;
-    state_         = state;
-  }
-
-private:
-  static Vec2 pos_, relativePos_;
-  static u32 state_, relativeState_;
 };
